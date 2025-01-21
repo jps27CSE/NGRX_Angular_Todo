dff549c8a27c5904825276a2107cfe2265ac7233
jps27cse <jackpritomsoren@hotmail.com>
2025-01-21 13:18:21 +0600

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,15 +8,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './store/reducers';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), // Optional for performance optimization
     provideRouter(routes), // Use routes defined in app.routes.ts
-    importProvidersFrom(
-      StoreModule.forRoot(reducers),
-      StoreDevtoolsModule.instrument({ maxAge: 25 }), // For Redux DevTools integration
-    ),
+    importProvidersFrom(StoreModule.forRoot(reducers)),
   ],
 };
